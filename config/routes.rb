adb17e419b351de8cad96e10ec1c4178c89bab88
ablakerose <bdecktor@gmail.com>
2020-03-03 15:43:23 -0500

    namespace resources and include session login and current_user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   # Want my API to be satisfiers.com/api/v1/resource 
   namespace :api do
       namespace :v1 do
         resources :users
+        resources :needs
+        resources :satisfiers
+        
       end
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
