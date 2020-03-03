d48a41490adb792eea53dd9b3770828c7043d3e3
ablakerose <bdecktor@gmail.com>
2020-03-03 17:08:12 -0500

    create user logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
-  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+ 
   # Want my API to be satisfiers.com/api/v1/resource 
   namespace :api do
       namespace :v1 do
