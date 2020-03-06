95bba0162e268068ea985ee29fd9dd06711e0ff7
ablakerose <bdecktor@gmail.com>
2020-03-06 10:08:29 -0500

    review and update information contained in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::API
 
     def logged_in?
         !!current_user
+        #if current_user is truthy-- !current_user flips false. !!current_user turns it back into true.
+        #so instead of the object, you're forcing it into a true or false value
     end
 
 end
