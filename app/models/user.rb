ea7f958b65b44539161c5aa3129fe3c525e7058c
ablakerose <bdecktor@gmail.com>
2020-03-06 10:11:35 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-    has_many :needs, through: :needs
-    has_many :satisifers
+    
+    has_many :satisfiers
+    has_many :needs, through: :satisfiers
+    
+
     has_secure_password
+    validates :name, :username, presence: true
+    validates :username, uniqueness: true
+
+    
 end
