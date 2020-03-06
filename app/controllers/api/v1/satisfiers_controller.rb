88c5f220a9d525aff02b8d61847741b489495afd
ablakerose <bdecktor@gmail.com>
2020-03-06 10:06:37 -0500

    test modifications in satisfiers controller to return data to user

--- app/controllers/api/v1/satisfiers_controller.rb
+++ app/controllers/api/v1/satisfiers_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::SatisfiersController < ApplicationController
+
+# def index
+    
+#     if logged_in? 
+#         needs = current_user.all
+
+#         render json: needs
+#     else render json: {
+#         error: "You must be logged in to see needs"
+#     }
+#     end
+# end
+
+
+
 end
