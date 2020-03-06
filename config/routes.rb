7808588e9f1567837795f2b7aa7aa67939250cc6
ablakerose <bdecktor@gmail.com>
2020-03-06 10:11:52 -0500

    add sign route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
    delete "/api/v1/logout", to: "api/v1/sessions#destroy"
    get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+
  
   # Want my API to be satisfiers.com/api/v1/resource 
   namespace :api do
       namespace :v1 do
-        resources :users
+        resources :users 
         resources :needs
         resources :satisfiers
         
