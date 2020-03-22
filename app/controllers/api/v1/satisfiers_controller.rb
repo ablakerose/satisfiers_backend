10beb56c76da0b3563a13e54364ffe5b419212fb
ablakerose <bdecktor@gmail.com>
2020-03-22 13:43:12 -0400

    modify create action in satisfiers controller to accept several need_ids and render those needs to update the need card

--- app/controllers/api/v1/satisfiers_controller.rb
+++ app/controllers/api/v1/satisfiers_controller.rb
@@ -14,33 +14,32 @@ def show
 end
 
 def create
-    
-    
-  satisfier = User.first.satisfiers.new(satisfier_params)
-    if satisfier.save
-      render json: satisfier
-    else 
-      render json: {error: 'Error creating satisfer'}
-    end
+#taking the array of needs from params and saving them to need_ids variable
+user = User.first
 
-  # if satisfier.save
-  #     render json: satisfier, status: created
-  # else
-  #     resp = {
-  #         error: satisfier.errors.full_messages.to_sentence
-  #     }
-  #     render json: user.errors, status: :unprocessable_entity
-  # end
+  need_ids = satisfier_params[:need_ids]
+  # create a satisfier associated to user
+  # including need_ids in sats_params will also associate that sat to those needs
+  user.satisfiers.create(satisfier_params)
+  # becasue what we need to send back is an array of updated needs
+  # find all of the needs that were just associated to the new satisfier
+  needs = Need.find(need_ids)
+  binding.pry
+   render json: needs, include: [:satisfiers]
+
 end
 
 
 private
 def satisfier_params
-    params.require(:satisfier).permit(:user_id, :activity, :value, needs_ids: [])
-    #make sure the nexted attribute doesn't have to be the join table of ways
-
+    params.require(:satisfier).permit(:user_id, :activity, :value, :need_ids => [])
   end
 
 
+  def need_params
+    params.require(:satisfier).permit(:need_ids => [])
+ 
+
+  end
 
 end
