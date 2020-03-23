8027e22271f9a0366154df478d302cc19f0c5950
ablakerose <bdecktor@gmail.com>
2020-03-23 15:37:08 -0400

    modify routes to eliminate unused routes

--- app/controllers/api/v1/satisfiers_controller.rb
+++ app/controllers/api/v1/satisfiers_controller.rb
@@ -14,9 +14,8 @@ def show
 end
 
 def create
-#taking the array of needs from params and saving them to need_ids variable
 user = User.first
-
+#taking the array of needs from params and saving them to need_ids variable
   need_ids = satisfier_params[:need_ids]
   # create a satisfier associated to user
   # including need_ids in sats_params will also associate that sat to those needs
@@ -24,7 +23,7 @@ user = User.first
   # becasue what we need to send back is an array of updated needs
   # find all of the needs that were just associated to the new satisfier
   needs = Need.find(need_ids)
-  binding.pry
+  
    render json: needs, include: [:satisfiers]
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
   
-  post "/api/v1/login", to: "api/v1/sessions#create"
-  post "/api/v1/signup", to: "api/v1/users#create"
-   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
-   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
-
  
   # Want my API to be satisfiers.com/api/v1/resource 
   namespace :api do
